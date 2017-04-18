ec22ebf3c70a2b79e44c8277b32b97f942a9079f
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-04-18 11:08:22 -0500

    quitar imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
+var cerrar = document.getElementsByClassName('cerrar');
 
+for(var i = 0; i < cerrar.length; i++){
+  cerrar[i].addEventListener('click',quitarImagen);
+};
+
+function quitarImagen(){
+  var padre = this.parentElement;
+  padre.style.display = 'none';
+}
